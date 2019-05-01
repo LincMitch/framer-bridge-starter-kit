ebc26ad5e8d751997916c6758eae2cdc17e0a3fc
Lincoln Mitchell <lincoln.mitchell@acularo.com>
2019-05-01 15:14:04 +1000

    add z

--- design-system/components/Button.tsx
+++ design-system/components/Button.tsx
@@ -29,7 +29,7 @@ export const Button: React.FC<Props> = ({
     className={`${fluid ? "fluid" : ""} ${disabled ? "disabled" : ""} ${
       kind ? kind : ""
     }`}
-  >ZZZ
+  >Z
     {text || children}
   </StyledButton>
 )
