2af255713679e80b751dc1f00e2d04af90a3e3cd
Lincoln Mitchell <lincoln.mitchell@acularo.com>
2019-05-08 14:16:03 +1000

    Fixed error on code/Chip, which also stopped Framer X22 from freezing.

--- design-system-v2.framerfx/code/Chip.tsx
+++ design-system-v2.framerfx/code/Chip.tsx
@@ -1,10 +1,7 @@
 import * as React from "react"
 import * as System from "../../design-system"
-import { ControlType, PropertyControls } from "framer"
 
 type Props = System.ChipProps & {
-  width: number
-  height: number
 }
 
 export class Chip extends React.Component<Props> {
@@ -13,17 +10,7 @@ export class Chip extends React.Component<Props> {
   }
 
   static defaultProps: Props = {
-    width: 150,
-    height: 48,
     label: "label",
   }
 
-  static propertyControls: PropertyControls<Props, any> = {
-    checkmark:	{ type: ControlType.Boolean, title: "Checkmark" },
-    icon:		{ type: ControlType.String, title: "Icon" },
-    id:		{ type: ControlType.String, title: "Id" },
-    label: { type: ControlType.String, title: "Label" },
-    selected:	{ type: ControlType.Boolean, title: "Selected" },	
-    trailingIcon:		{ type: ControlType.String, title: "TrailingIcon" },
-  }
 }
