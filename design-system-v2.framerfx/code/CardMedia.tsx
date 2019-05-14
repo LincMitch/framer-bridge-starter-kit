9eba63548c8ebfd59e726e88bd82b2f49d65ced5
Lincoln Mitchell <lincoln@identitii.com>
2019-05-14 13:28:39 +1000

    added CardMedia image via styled components and added prop

--- design-system-v2.framerfx/code/CardMedia.tsx
+++ design-system-v2.framerfx/code/CardMedia.tsx
@@ -3,36 +3,46 @@ import * as System from "../../design-system"
 import { ControlType, PropertyControls } from "framer"
 import { cloneFrameless } from "../../design-system-v2.framerfx/node_modules/@framer/lintonye.learnreactdesign-ds/code/tools/framerx-utils";
 import FramerXWrapper from "./FramerXWrapper"
+import styled from 'styled-components'
+
+
 
 type Props = System.CardMediaProps & {
   object: string[];
   externalObject: React.ReactNode;
   activeObjectIndex: number;
+  image: string
 }
+
+
+const StyledCardMedia = styled(System.CardMedia)`
+color: red;
+  background-repeat: no-repeat;
+  background-position: center;
+  height: 300px;
+  background-image: ${props => props.image};
+`
 
 export class CardMedia extends React.Component<Props> {
   render() {
-    const { object, externalObject, ...rest } = this.props;
+    const { image, object, externalObject, ...rest } = this.props;
 
     let objectElements;
     objectElements = cloneFrameless(externalObject); 
-    return (<System.CardMedia {...this.props} style={{
-      backgroundImage: 'url(https://rmwc.io/images/backgrounds/mb-bg-fb-16.png)',
-      backgroundRepeat: 'no-repeat',
-      backgroundPosition: 'center',
-      height: '300px'
-    }}
-    >{objectElements}</System.CardMedia>)
+    return (<StyledCardMedia {...this.props}
+    >{objectElements}</StyledCardMedia>)
   }
 
   static defaultProps: Props = {
     sixteenByNine: false,
-    activeObjectIndex: 0
+    activeObjectIndex: 0,
+    image: "url('https://rmwc.io/images/backgrounds/mb-bg-fb-16.png')"
   }
 
   static propertyControls: PropertyControls<Props> = {
     sixteenByNine: { type: ControlType.Boolean, title: "SixteenByNine" },
     square: { type: ControlType.Boolean, title: "Square" },
+    image: { type: ControlType.String, title: "Image"},
     
     externalObject: {
       type: ControlType.ComponentInstance,
@@ -45,3 +55,5 @@ export class CardMedia extends React.Component<Props> {
     }
   }
 }
+
+
