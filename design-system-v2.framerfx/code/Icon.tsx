192d2a016f680e1765c8fb208509ef7defff90e6
Lincoln Mitchell <lincoln@identitii.com>
2019-07-15 13:20:42 +1000

    Adding theme to icons

--- design-system-v2.framerfx/code/Icon.tsx
+++ design-system-v2.framerfx/code/Icon.tsx
@@ -1,23 +1,32 @@
 import * as React from "react"
 import * as System from "../../design-system"
 import { ControlType, PropertyControls } from "framer"
+import FramerXWrapper from "./FramerXWrapper"
+
+import {
+  themePropertyControls,
+} from "./framerx-integration";
 
 type Props = System.IconProps & {
 }
 
 export class Icon extends React.Component<Props> {
   render() {
-    return <System.Icon {...this.props} />
+    const { icon, ...rest } = this.props;
+
+    return (
+      <FramerXWrapper>
+        <System.Icon {...this.props}/>
+      </FramerXWrapper>
+    )
   }
 
   static defaultProps: Props = {
     icon: "star",
-    theme: ""
   }
 
   static propertyControls: PropertyControls<Props> = {
     icon: { type: ControlType.String, title: "Icon" },
- 
-    theme: { type: ControlType.String, title: "Theme" }
+    ...themePropertyControls("typography")
   }
 }
