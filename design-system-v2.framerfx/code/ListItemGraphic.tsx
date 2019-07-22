93c804a80362ff44ae4ec0326760c8a9a8bdad29
Lincoln Mitchell <lincoln@identitii.com>
2019-07-22 18:28:38 +1000

    Added theme, paddign and margin to ListItemGraphic

--- design-system-v2.framerfx/code/ListItemGraphic.tsx
+++ design-system-v2.framerfx/code/ListItemGraphic.tsx
@@ -1,6 +1,13 @@
 import * as React from "react"
 import * as System from "../../design-system"
 import { ControlType, PropertyControls } from "framer"
+import FramerXWrapper from "./FramerXWrapper";
+import {
+  themePropertyControls,
+  spacingPropertyControls,
+  processSpacingProps
+} from "./framerx-integration";
+
 
 type Props = System.ListItemGraphicProps & {
 
@@ -9,7 +16,11 @@ type Props = System.ListItemGraphicProps & {
 export class ListItemGraphic extends React.Component<Props> {
   render() {
     const {  ...rest } = this.props;
-    return <System.ListItemGraphic {...this.props} />
+    return (
+      <FramerXWrapper>
+        <System.ListItemGraphic {...this.props} />
+      </FramerXWrapper>
+    )
   }
 
   static defaultProps: Props = {
@@ -18,5 +29,7 @@ export class ListItemGraphic extends React.Component<Props> {
 
   static propertyControls: PropertyControls<Props> = {
     icon: { type: ControlType.String, title: "Icon" },   
+    ...themePropertyControls("typography"),
+    ...spacingPropertyControls()
   }
 }
